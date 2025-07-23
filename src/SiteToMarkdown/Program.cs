ee2f2a1be24d11582bdbb6738a66de6a286c5989
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2025-07-23 11:07:07 +0200

    Move logic into smaller functions
    Moved most of the logic behind the scraper into the `Utils` class,
    making the `Program.cs` much more approachable and readable
    This also enables finer testing of each chunk of the logic

--- src/SiteToMarkdown/Program.cs
+++ src/SiteToMarkdown/Program.cs
@@ -16,7 +16,7 @@ if (parseResult is not Parsed<Options> parsed)
 var url = parsed.Value.Url;
 if (!Uri.TryCreate(url, UriKind.Absolute, out var uri))
 {
-    throw new ArgumentException($"Please provice a valid URL. Provided: {url}");
+    throw new ArgumentException($"Please provice a valid absolute URL. Provided: {url}");
 }
 
 var web = new HtmlWeb();
@@ -30,98 +30,15 @@ var converter = new ReverseMarkdown.Converter(new ReverseMarkdown.Config
 });
 
 var markdownBuilder = new StringBuilder();
-var mdFilename = UriToMarkdownFileName(uri);
+var mdFilename = Utils.UriToMarkdownFileName(uri);
 
-var pagesDiscovered = new HashSet<Uri>();
-var pagesToScrape = new Queue<Uri>();
-pagesToScrape.Enqueue(uri);
+var docs = Utils.ScrapeUrl(web, uri);
+var converted = docs
+    .Select(static doc => doc.DocumentNode.InnerHtml)
+    .Select(converter.Convert);
 
-while (pagesToScrape.Count > 0)
-{
-    var currentUrl = pagesToScrape.Dequeue();
-    if (pagesDiscovered.Contains(currentUrl))
-    {
-        continue;
-    }
-
-    pagesDiscovered.Add(currentUrl);
-
-    HtmlDocument? doc;
-    try
-    {
-        Console.WriteLine($"Loading {currentUrl}");
-        doc = web.Load(currentUrl);
-    }
-    catch
-    {
-        Console.WriteLine($"Failed to load {currentUrl}");
-        continue;
-    }
-
-    if (doc.DocumentNode is null)
-    {
-        continue;
-    }
-
-    var md = converter.Convert(doc.DocumentNode.InnerHtml);
-    markdownBuilder.AppendLine(md);
-
-    var links = doc.DocumentNode.SelectNodes("//a[@href]");
-    if (links is null)
-    {
-        continue;
-    }
-
-    foreach (var link in links)
-    {
-        var href = link.GetAttributeValue("href", string.Empty);
-        if (string.IsNullOrEmpty(href)
-            || !Uri.TryCreate(href, UriKind.RelativeOrAbsolute, out var result)
-            || !uri.IsBaseOf(result)
-            || result.OriginalString == "/" // This feels like a hack...
-            || IsOnlyFragment(result))
-        {
-            continue;
-        }
-
-        if (!result.IsAbsoluteUri)
-        {
-            result = new Uri(uri, result);
-        }
-
-        if (pagesDiscovered.Contains(result))
-        {
-            continue;
-        }
-
-        pagesToScrape.Enqueue(result);
-    }
-}
+markdownBuilder.Append(string.Join("\n\n", converted));
 
 using var writer = new StreamWriter(mdFilename, false, Encoding.UTF8);
 writer.Write(markdownBuilder.ToString());
 Console.WriteLine($"Wrote {markdownBuilder.Length} characters to {mdFilename}");
-
-static string UriToMarkdownFileName(Uri uri)
-{
-    var segments = uri.Segments
-        .Where(s => !string.IsNullOrWhiteSpace(s) && s != "/")
-        .Select(s => s.Trim('/'))
-        .ToList();
-
-    if (segments.Count == 0)
-    {
-        return $"{uri.Host}.md";
-    }
-
-    var segmentsString = string.Join('.', segments);
-    return $"{uri.Host}.{segmentsString}.md";
-}
-
-// TODO: Is this the best way to check this?
-static bool IsOnlyFragment(Uri uri)
-{
-    return uri is not null
-        && !uri.IsAbsoluteUri
-        && uri.OriginalString.StartsWith('#');
-}
---
+++ src/SiteToMarkdown/Utils.cs
@@ -0,0 +1,123 @@
+﻿using HtmlAgilityPack;
+
+namespace SiteToMarkdown;
+
+public static class Utils
+{
+    public static string UriToMarkdownFileName(Uri uri)
+    {
+        var segments = uri.Segments
+            .Where(s => !string.IsNullOrWhiteSpace(s) && s != "/")
+            .Select(s => s.Trim('/'))
+            .ToList();
+
+        if (segments.Count == 0)
+        {
+            return $"{uri.Host}.md";
+        }
+
+        var segmentsString = string.Join('.', segments);
+        return $"{uri.Host}.{segmentsString}.md";
+    }
+
+    // TODO: Is this the best way to check this?
+    public static bool IsOnlyFragment(Uri uri)
+    {
+        return uri is not null
+            && !uri.IsAbsoluteUri
+            && uri.OriginalString.StartsWith('#');
+    }
+
+    public static bool TryLoadUrl(HtmlWeb web, Uri url, out HtmlDocument? doc)
+    {
+        doc = null;
+        if (!url.IsAbsoluteUri)
+        {
+            return false;
+        }
+
+        try {
+            doc = web.Load(url);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    public static void EnqueueRelevantLinks(
+        HtmlDocument doc,
+        Uri baseUrl,
+        HashSet<Uri> pagesDiscovered,
+        Queue<Uri> pagesToScrape)
+    {
+        ArgumentNullException.ThrowIfNull(doc);
+        ArgumentNullException.ThrowIfNull(doc.DocumentNode);
+
+        if (doc.DocumentNode.SelectNodes("//a[@href]") is not { Count: > 0 } links)
+        {
+            return;
+        }
+
+        foreach(var link in links)
+        {
+            var href = link.GetAttributeValue("href", string.Empty);
+            if (string.IsNullOrEmpty(href)
+                || !Uri.TryCreate(href, UriKind.RelativeOrAbsolute, out var result)
+                || !baseUrl.IsBaseOf(result)
+                || result.OriginalString == "/" // This feels like a hack...
+                || IsOnlyFragment(result))
+            {
+                continue;
+            }
+
+            if (!result.IsAbsoluteUri)
+            {
+                // Turn `result` into an absolute Uri
+                result = new Uri(baseUrl, result);
+            }
+
+            if (pagesDiscovered.Contains(result))
+            {
+                continue;
+            }
+
+            pagesToScrape.Enqueue(result);
+        }
+    }
+
+    public static IEnumerable<HtmlDocument> ScrapeUrl(HtmlWeb web, Uri url)
+    {
+        ArgumentNullException.ThrowIfNull(web);
+        ArgumentNullException.ThrowIfNull(url);
+        if (!url.IsAbsoluteUri)
+        {
+            yield break;
+        }
+
+        var pagesDiscovered = new HashSet<Uri>();
+        var pagesToScrape = new Queue<Uri>();
+        pagesToScrape.Enqueue(url);
+
+        while (pagesToScrape.Count > 0)
+        {
+            var currentUrl = pagesToScrape.Dequeue();
+            if (pagesDiscovered.Contains(currentUrl))
+            {
+                continue;
+            }
+            pagesDiscovered.Add(currentUrl);
+
+            Console.WriteLine($"Loading {currentUrl}");
+            if (!TryLoadUrl(web, currentUrl, out var doc)
+                || doc is null
+                || doc.DocumentNode is null)
+            {
+                Console.WriteLine($"Failed to load {currentUrl}");
+                continue;
+            }
+
+            EnqueueRelevantLinks(doc, url, pagesDiscovered, pagesToScrape);
+            yield return doc;
+        }
+    }
+}
